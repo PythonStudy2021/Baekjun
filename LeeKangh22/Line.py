de51bb7df86abed5360dcd26df73835846cd3f2f
KANG HEE LEE <67950809+LeeKangh22@users.noreply.github.com>
2021-01-18 16:39:31 +0900

    solving 2358

---
+++ LeeKangh22/Line.py
@@ -0,0 +1,12 @@
+n=int(input())
+spot=[n]
+county=0
+sum=0
+for i in range(0,n):
+    x,y=map(int,input().split())
+    spot.append([x,y])
+for i in range(0,n):
+    county=spot[i][0]
+    for j in range(i,n):
+        if spot[i][0]==spot[j][0]:
+            sum+=1
