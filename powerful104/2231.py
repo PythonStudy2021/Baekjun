5f82faefd590e95c9c45d09fc498e7daf31feddb
Park Sanghyun <76836510+powerful104@users.noreply.github.com>
2021-02-05 02:41:47 +0900

    Add files via upload

--- powerful104/2231.py
+++ powerful104/2231.py
@@ -1,9 +1,10 @@
 num = int(input())
 ans=0
-for i in range(1,num+1):
-    su=0
-    su = sum(map(int,list(str(i))))
-    if i+su==num:
-        ans=i
-        break
+for i in range(num-len(str(num))*9,num+1):
+    if i>0:
+        su=0
+        su = sum(map(int,list(str(i))))
+        if i+su==num:
+            ans=i
+            break
 print(ans)
