2f38dc15700f55092f187de6921bdf23e8314ee5
KANG HEE LEE <67950809+LeeKangh22@users.noreply.github.com>
2021-02-09 14:09:55 +0900

    solved by Leekh #retry

--- LeeKangh22/1436.py
+++ LeeKangh22/1436.py
@@ -1,4 +1,10 @@
-end="666"
-N=int(input())
-N-=1
-print(int(str(N)+end))
+n = int(input())
+cnt = 0
+six_n = 666
+while True:
+    if '666' in str(six_n):
+        cnt += 1
+    if cnt == n:
+        print(six_n)
+        break
+    six_n += 1
