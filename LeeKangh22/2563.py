1d71ffecfafb3c6225f018ff9752b6394b0fdf94
KANG HEE LEE <67950809+LeeKangh22@users.noreply.github.com>
2021-02-09 14:07:57 +0900

    solved by Leekh #retry

--- LeeKangh22/2563.py
+++ LeeKangh22/2563.py
@@ -1,6 +1,13 @@
-def in_range(n,start,end):
-    return start<=n<=end if end>=strat else end<=n<=start
+N = int(input())
+paper = [[0 for _ in range(101)] for _ in range(101)]
 
-N=int(input())
-for i in range(0,3):
-    x,y=map(int,input().split())
+for _ in range(N):
+    x, y = map(int, input().split())
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            paper[i][j] = 1
+
+answer = 0
+for row in paper:
+    answer += row.count(1)
+print(answer)
