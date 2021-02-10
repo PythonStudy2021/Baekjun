7c52b6ec91470c570360c35bf18401f7ee289131
KANG HEE LEE <67950809+LeeKangh22@users.noreply.github.com>
2021-02-10 15:40:40 +0900

    solved 1010 by Leekh #retry

--- LeeKangh22/1010.py
+++ LeeKangh22/1010.py
@@ -1,7 +1,12 @@
-T=int(input())
-answer=1
-for i in range(0,T):
-    N,M=map(int,input().split())
-    for j in range(1,N+1):
-        for k in range(j,j+N-1)
+T = int(input())
+for _ in range(T):
+    m, n = map(int, input().split())
+    answer = 1
+    k = n - m
+    while n > k:
+        answer *= n
+        n -= 1
+    while m > 1:
+        answer = answer // m
+        m -= 1
     print(answer)
