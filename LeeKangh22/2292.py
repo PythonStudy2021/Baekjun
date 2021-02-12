0604ef3bfd94b26dcf041976c1a516861cccf626
KANG HEE LEE <67950809+LeeKangh22@users.noreply.github.com>
2021-02-12 18:10:17 +0900

    solved 2292 by Leekh #retry

--- LeeKangh22/2292.py
+++ LeeKangh22/2292.py
@@ -1,2 +1,14 @@
-n=int(input())
-#solving
+N = int(input())
+first = 1
+plus = 6
+room = 1
+if N == 1:
+    print(1)
+else:
+    while True:
+        first = first + plus
+        room+= 1
+        if N <= first:
+            print(room)
+            break
+        plus += 6
